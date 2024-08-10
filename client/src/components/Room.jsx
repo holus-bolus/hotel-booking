d2844548504bdd56b85535732ca13f2a5aafc614
holus-bolus <ustinovoleksij@gmail.com>
2024-08-10 16:49:04 +0300

    Added the modal window;

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
 const Room = ({ room }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div className="row">
       <div className="col-md-4">
@@ -14,10 +22,27 @@ const Room = ({ room }) => {
             <p className="card-text">{room.phonenumber}</p>
             <p className="card-text">{room.description}</p>
             <p className="card-text">${room.pricePerNight} / night</p>
-            <button className="btn btn-primary">More Info</button>
+            <button className="btn btn-primary" onClick={handleShow}>
+              More Info
+            </button>
           </div>
         </div>
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
